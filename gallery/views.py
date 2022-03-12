3f49475af3f8507ea77066510f5fc1ab16d27cde
Jernskegg <casper.hille@outlook.com>
2022-03-12 18:15:03 +0100

    Build: Add view for Gallery model

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
+from gallery.models import imageEntry
 
-# Create your views here.
+class imageList(generic.ListView):
+    model = imageEntry
+    context_object_name = 'images'
+    queryset = imageEntry.objects.order_by('-date_posted')
+    template_name = 'gallery.html'
