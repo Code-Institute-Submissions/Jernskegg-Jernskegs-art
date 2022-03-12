2357f089cc385b7044c2e524b84d1eb88b51a0d2
Jernskegg <casper.hille@outlook.com>
2022-03-12 18:14:40 +0100

    Build: Add Adminpage for gallery model

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import imageEntry
 
-# Register your models here.
+
+class imageEntry_admin(admin.ModelAdmin):
+
+    list_display = ('title', 'date_posted', 'price')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title', 'image']
+    list_filter = ('date_posted','price' )
+
+
+
+admin.site.register(imageEntry, imageEntry_admin)
