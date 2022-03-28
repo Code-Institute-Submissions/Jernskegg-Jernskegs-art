bb3f2548bf13e351a89049e937f6ee15dccbabc0
Jernskegg <casper.hille@outlook.com>
2022-03-29 01:40:06 +0200

    Build: Add html, view  and urls for checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ImageOrderForm, RequestOrderForm
 
-# Create your views here.
+
+def image_checkout(request):
+    cart = request.session.get('cart', [])
+    if not cart:
+        return (redirect('cart'))
+
+    image_order_form = ImageOrderForm()
+
+    context = {
+        'image_order_form': image_order_form,
+    }
+
+    return render(request, 'checkout.html', context)
+
+
+def request_checkout(request):
+    request_cart = request.session.get('request_cart', [])
+    if not request_cart:
+        return (redirect('request'))
+
+    request_order_form = RequestOrderForm()
+
+    context = {
+        'request_order_form': request_order_form,
+    }
+
+    return render(request, 'checkout.html', context)
